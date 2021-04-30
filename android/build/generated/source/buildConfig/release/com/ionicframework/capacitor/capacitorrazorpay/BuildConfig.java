546502b7a8ad537e0246b6113f020baf244c683a
Atish Abhang <atishabhang@live.com>
2021-05-01 01:28:34 +0530

    update

---
+++ android/build/generated/source/buildConfig/release/com/ionicframework/capacitor/capacitorrazorpay/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.ionicframework.capacitor.capacitorrazorpay;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.ionicframework.capacitor.capacitorrazorpay";
+  public static final String BUILD_TYPE = "release";
+}
